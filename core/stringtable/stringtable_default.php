b09633f88b78fb6fc82d586c5b6388adb9af1ea1
Vojtěch Václav Porteš <info@justart.org>
2015-12-06 00:41:57 +0100

    Update of detaul module not found error

--- core/stringtable/stringtable_default.php
+++ core/stringtable/stringtable_default.php
@@ -4,7 +4,7 @@ $Stringtable = array(
 	'default_heading' => 'Test',
 	'default_cms_name' => 'CMShadow<sup>3</sup>',
 	'default_module_right_error' => '<p>Sorry, You don\'t have enough rights to access module <strong>%s</strong>.</p>',
-	'default_module_not_found_error' => '<p>Sorry, module <strong>%s</strong> was not found.</p>',
+	'default_module_not_found_error' => '<p>Sorry, module <strong>%s</strong> was not found or contains errors.</p><div class="bs-callout bs-callout-danger"><h3>Details:</h3>%s</div>',
 	'default_node_right_error' => '<p>Sorry, You don\'t have enough rights to access this page.<br><a href="'.ADMIN_PATH.'">Go back to the <strong>admin homepage</strong></a>.</p>',
 	'default_file_missing_error' => '<p>Sorry, requested file <strong>%s</strong> doesn\'t exist. If you think this is error, please contact system administrator.</p>'
 )
