39e0e3011534bf6ccbd4cde6a58c7f140c54d190
Vojtěch Václav Porteš <info@justart.org>
2015-12-05 17:04:03 +0100

    Custom script loader

--- style/script/main.js
+++ style/script/main.js
@@ -1,17 +1,45 @@
 var uid = 'cmshadow3';
 var iid = 0;
+var baseUrl = '/style/script/';
 
 require.config({
-    baseUrl: '/style/script/',
+    baseUrl: baseUrl,
     urlArgs: "d=" + (new Date()).getTime(),
     paths: {
-        bootstrap: 'bootstrap',
+        'bootstrap': 'bootstrap',
+        'aplication-modules': 'application-modules'
     }
 });
 
+var customScriptLoader = {
+    'require': function(callback){
+        var scripts = this.getScripts();
+        if (scripts) {
+            require(scripts, callback);
+        }
+    },
+    'getScripts': function(){
+        var $scripts = $('#__customScripts'), scripts = [];
+        if ($scripts.length === 1) {
+            $.each($scripts.data('scripts')[0], function(k, el){
+                scripts[k] = el.replace(baseUrl.substring(1,baseUrl.length), '').replace('.js', '');
+            });
+
+            return scripts;
+        }
+        return false;
+    }
+}
+
 require(['jquery'], function($) {
 	require(['api'], function(){
-		require(['application']);
+		require(['application'], function(){
+            $(document).ajaxStop(function(){
+                customScriptLoader.require(function(){
+                    console.log('Custom Scripts loaded');
+                });
+            });
+        });
 	});
     require(['bootstrap/dropdown', 'bootstrap/modal', 'bootstrap/navigation', 'boostrap/affix', 'bootstrap/dimension', 'bootstrap/transition', 'bootstrap/collapse'], function($) {
         console.log('BS loaded!')
