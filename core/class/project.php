61ccd76e28b7f3dbf4745c5f81154b9c4b374748
Vojtěch Václav Porteš <info@justart.org>
2015-12-06 01:02:25 +0100

    Addition of Project class (markup)

---
+++ core/class/project.php
@@ -0,0 +1,55 @@
+<?php
+
+Class Project Extends Minimal {
+
+	public $name;
+	public $description;
+	public $owners;
+	public $editors;
+	public $releasedate;
+
+	public createProject () {
+
+		// return lastid
+	}
+
+	public updateProject () {
+
+	}
+
+	private concateProjectOwners () {
+		
+	}
+
+	public setProjectOwners () {
+
+		// return rows
+	}
+
+	public addProjectOwners () {
+
+	}
+
+	public removeProjectOwners () {
+
+	}
+
+	public getProjects () {
+
+	}
+
+	public getProjectByID  ($id) {
+
+	}
+
+	public getProjectByUser ($id) {
+
+	}
+
+	public getProjectByUsers ($users) {
+
+	}
+
+}
+
+?>
