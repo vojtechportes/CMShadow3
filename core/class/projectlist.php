1894d3d9c770f5bd116b1bcfdc06a2213a44fd50
Vojtěch Václav Porteš <info@justart.org>
2015-12-06 20:42:08 +0100

    Addition of projectlist class

---
+++ core/class/projectlist.php
@@ -0,0 +1,23 @@
+<?php
+
+Class ProjectList Extends Module {
+
+	public $templateOutput = DEFAULT_OUTPUT;
+	public $template = '/admin/project/list';	
+
+	public function __construct ($action, $id = 0, $limit = array(0, 20)) {
+		switch ($action) {
+			case 'getProjectList':
+				$Projects = new Project();
+				$ProjectList = $Projects->getProjects($limit);
+				$ProjectCount = $Projects->getProjectCount();
+
+				$this->template = '/admin/project/list';
+				$this->output['projects'] = $ProjectList;
+				$this->output['count'] = $ProjectCount['ProjectCount'];
+				break;
+		}
+	}
+}
+
+?>
