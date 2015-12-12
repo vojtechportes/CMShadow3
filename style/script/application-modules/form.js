8ffc089e31f9da52e68e1e1704a629e8ecdce341
Vojtěch Václav Porteš <info@justart.org>
2015-12-13 00:08:32 +0100

    forms.js update

--- style/script/application-modules/form.js
+++ style/script/application-modules/form.js
@@ -1,3 +1,7 @@
+function APITest (data, load, html, e) {
+	console.log(data);
+}
+
 (function(){
 	var form = function () {
 		var parent = '.formLoader[data-api-load]';
@@ -11,13 +15,39 @@
 					parentData = $el.data('api-load');
 
 				if ($form.length > 0) {
-				
 					$form.off('submit.formloader').on('submit.formloader', function(e){			
 						var formData = $form.formatForAPI();
-						parentData['arguments']['formData'] = formData;
-						APICommands.call($parent, parentData, true);
-						
+						parentData = $.extend({}, parentData, formData, {'form_open': 1});
+
+						$.post('/admin/api/', parentData)
+							.done(function(data) {
+								$.each(data, function(k, module){
+									$el.html(module['__html']);
+								});	
+
+								$(window).trigger('apiReload.admin/form');
+
+								if (typeof parentData['arguments']['dependencies'] !== 'undefined') {
+									$.each(parentData['arguments']['dependencies'], function(k, module){
+										$(window).trigger('apiReloadForce.' + module);
+									});
+								}
+							})
+							.fail(function(data) {
+								CMSAPI.setStatus(data);
+							});
+
 						e.preventDefault();
+						e.stopPropagation();
+						return false;
+					});
+
+					$el.on('click', '[data-api-reload]', function(e){
+						e.preventDefault();
+						e.stopPropagation();
+
+						var parentData = $.extend({}, $el.data('api-load'), {'form_open': 1});
+						APICommands.call($el, parentData, true);
 					});
 				}
 			});
@@ -28,7 +58,7 @@
 		form();
 	});
 
-	$(window).on('apiReload.admin/*/*/form', function(){
+	$(window).on('apiReload.admin/form', function(){
 		form();
 	});	
 })();
