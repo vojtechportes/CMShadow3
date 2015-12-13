c4a2b9dcf7de6ec5180ceda0183c4d0c88e6e512
Vojtěch Václav Porteš <info@justart.org>
2015-12-13 21:57:28 +0100

    Addition of projectlist.js module

---
+++ style/script/application-modules/projectlist.js
@@ -0,0 +1,14 @@
+(function(){
+	var projectListReload = function () {
+		var parent = '.projectList[data-api-load]';
+		$.each($(parent), function(k, el){
+			console.log(el);
+			var parentData = $('.projectList' + parent).data('api-load');
+			APICommands.call($(el), parentData, true);
+		});		
+	}
+
+	$(window).on('apiReloadForce.admin/project/list', function(){
+		projectListReload();
+	});
+})();
