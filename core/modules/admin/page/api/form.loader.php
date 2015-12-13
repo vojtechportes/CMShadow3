36b9b6e43d63a5b1f903361b329259a669bd9300
Vojtěch Václav Porteš <info@justart.org>
2015-12-13 22:01:45 +0100

    Update of page form loader
    Addition of Path property to form loader arguments

--- core/modules/admin/page/api/form.loader.php
+++ core/modules/admin/page/api/form.loader.php
@@ -1,9 +1,10 @@
 <?php
+global $Path;
 
 $Module = new Module();
 $Loader = new APIOutput();
 $Loader->template = '/admin/page/api/form';
-$Module->addModule($Loader, $return);
+$Module->addModule($Loader, $return + array("Path" => $Path));
 $Module->output(false);
 
 ?>
