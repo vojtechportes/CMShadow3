bee544d6d198560d1b5765c7901b2197fef8bc64
Vojtěch Václav Porteš <info@justart.org>
2015-12-15 18:46:50 +0100

    Update of workflow module
    Addition of transition steps validation and rerendering of module when
    transition is done.

--- core/modules/admin/project/workflow.php
+++ core/modules/admin/project/workflow.php
@@ -4,6 +4,21 @@ global $M;
 $id = $M->extractID($return['Path']);
 $_Project = new Project();
 $Project = $_Project->getProjectByID($id);
+$ProjectRights = array();
+
+if ((bool) $Project['HasAdminRights'])
+	$ProjectRights[] = 1;
+if ((bool) $Project['HasEditorRights'])
+	$ProjectRights[] = 2;
+
+if ($val = filter_input(INPUT_POST, 'changeState', FILTER_VALIDATE_INT)) {
+	$ProjectWorkflow = new ProjectWorkflow();
+	$ProjectWorkflow->state = $val;
+	$ProjectWorkflow->id = $id;
+	$ProjectWorkflow->projectRights = $ProjectRights;
+	$ProjectWorkflow->changeState();
+	$Project = $_Project->getProjectByID($id);
+}
 
 if (empty($Project) || (int) $Project['HasRights'] === 0) {
 	if (!empty($Project)) { $msg = "{_'forms_project_form_cant_modify'}"; } else { $msg = "{_'forms_project_form_doesnt_exist'}"; }
@@ -12,7 +27,10 @@ if (empty($Project) || (int) $Project['HasRights'] === 0) {
 	$Message->addModule(new Message(), array("html" => $msg, "class" => "alert-danger", "OutputStyle" => $return["OutputStyle"], "OutputType" => $return["OutputType"], "Header" => 200));
 	$Message->output();				
 } else {
-	$return['workflow'] = $_Project->getProjectWorkflow()[$Project['Status']];
+	$Workflow = $_Project->getProjectWorkflow()[$Project['Status']];
+	$Workflow = $_Project->checkProjectWorkflowRights($Workflow, $ProjectRights);
+
+	$return['workflow'] = $Workflow;
 	$return['status'] = (int) $Project['Status'];
 	
 	$Module = new Module();
