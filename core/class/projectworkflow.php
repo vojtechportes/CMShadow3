9fdcc65eca39854166716b27003590698463823c
Vojtěch Václav Porteš <info@justart.org>
2015-12-15 19:00:56 +0100

    Code optimisation and workflow validation

--- core/class/projectworkflow.php
+++ core/class/projectworkflow.php
@@ -15,41 +15,45 @@ Class ProjectWorkflow Extends Project {
 	}
 
 	public function changeState () {
+		if (!$this->checkProjectWorkflowRights())
+			return false;
+
 		switch ($this->state) {
 			case '1':
 			case '2':
 				/* Will unlock the project if project is locked */
-				$this->changeProjectState();
+
 				break;
 
 			case '3':
 			case '4':
 				/* In this two states, project is locked and is waiting for actions 5 or 6 */
-				$this->changeProjectState();
+
 				break;
 
 			case '5':
 				/* Will release project, copy all changes to zero version of pages. This change can be taken back by rollback. Project can be also reopened. */
-				$this->changeProjectState();
+
 				break;
 
 			case '6':
 				/* Will remove all pages from project and unlock zero version of pages so they are available. This step is final. */
-				$this->changeProjectState();
+
 				break;
 
 			case '7':
 				/* Will rollback release of the project if is last project made and keep project in state ready for release */
 				$this->state = 3;
-				$this->changeProjectState();
 				break;
 
 			case '8':
 				/* Will create new project and keep pages locked or fail if pages are in different project already */
 				$this->state = 1;
-				$this->changeProjectState();
 				break;
 		}
+
+		$this->changeProjectState();
+		return true;
 	}
 
 }
