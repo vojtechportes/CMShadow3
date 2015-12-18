3a6730608e4d168dffd87ae58290139efcd9f0cc
Vojtěch Václav Porteš <info@justart.org>
2015-12-18 22:53:39 +0100

    Addition of stringtable_parsedown

---
+++ core/stringtable/stringtable_parsedown.php
@@ -0,0 +1,24 @@
+<?php
+
+$Stringtable = array(
+	'parsedown_heading' => 'Parsedown help',
+	'parsedown_legend_strong_left' => '__some text__',
+	'parsedown_legend_strong_right' => 'Wrapping text to __ characters will make it bold',
+	'parsedown_legend_link_left' => 'http://somelink.com',
+	'parsedown_legend_link_right' => 'Link in propper format will be automaticly transformed to anchor',
+	'parsedown_legend_email_left' => 'johndoe@somedomain.com',
+	'parsedown_legend_email_right' => 'Email in propper format will be automaticly transformed to anchor',
+	'parsedown_legend_headings_left' => '# Heading h1<br>## Heading H2<br>### Heading H3',
+	'parsedown_legend_headings_right' => 'Using # character before text will transform to heading. Using one to six # characters will transform it to headings from first to sixth level. ',
+	'parsedown_legend_unordered_list_left' => '- list item<br>- list item<br>- list item',
+	'parsedown_legend_unordered_list_right' => 'Using dash before text will make it unordered list. Each line starting with dash will be transformed to one item of list',
+	'parsedown_legend_ordered_list_left' => '1. list item<br>2. list item<br>3. list item',
+	'parsedown_legend_ordered_list_right' => 'Using numbers followed by dot followed by text will be transformed to ordered list',
+	'parsedown_legend_table_left' => 'Cell heading | Cell heading<br>------------ | ------------<br>Cell item   | Cell item  <br>Cell item   | Cell item   ',
+	'parsedown_legend_table_right' => 'Text formated like this will be transformed to table. dashed on second line has to match number of characters in first line.',
+	'parsedown_legend_user_mention_left' => '@user_name',
+	'parsedown_legend_user_mention_right' => 'Using @ characters followed by user name will be transformed to link to user account'
+)
+
+?>
+
