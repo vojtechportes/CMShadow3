4397451c4c8a5c44df08e0145052dd1a1418aee7
Vojtěch Václav Porteš <info@justart.org>
2015-12-19 02:16:38 +0100

    Addition of styleInput attribute to textarea

--- core/class/formelement_textarea.php
+++ core/class/formelement_textarea.php
@@ -4,7 +4,7 @@ Class FormElement_Textarea Extends FormElement {
 	public function output () {
 		$str = '';
 
-		$str = '<textarea class="'.$this->getAttribute("classInput").'" id="'.$this->getAttribute("id").'" name="'.$this->getAttribute("name").'"';
+		$str = '<textarea class="'.$this->getAttribute("classInput").'" style="'.$this->getAttribute("styleInput").'" id="'.$this->getAttribute("id").'" name="'.$this->getAttribute("name").'"';
 		if ((bool) $this->getAttribute("required"))
 			$str .= ' required';
 		if ($this->getAttribute("placeholder"))
