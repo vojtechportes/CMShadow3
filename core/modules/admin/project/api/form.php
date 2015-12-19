b202ed00fc4669662e0df44b27e8b53345ded00c
Vojtěch Václav Porteš <info@justart.org>
2015-12-19 02:27:05 +0100

    Update of project api form
    Updated by page select and functionality that can add pages to project.

--- core/modules/admin/project/api/form.php
+++ core/modules/admin/project/api/form.php
@@ -34,6 +34,17 @@ if (array_key_exists('type', $return)) {
 				$OwnerValues = flatten_array($OwnerValues);
 				$EditorValues = $_Project->getProjectOwners($Project['ID'], 2);
 				$EditorValues = flatten_array($EditorValues);
+
+				$Pages = new Page();
+				$Pages->version = $Project['ID'];
+				$Pages->getPageTree(false, 0);
+				$PageTree = $Page->pagetTree;
+				$PageValues = array();
+				if (count($PageTree) > 0) {
+					foreach ($PageTree as $page) {
+						array_push($PageValues, $page['ID']);
+					}
+				}
 			}
 		}
 	}
@@ -60,6 +71,21 @@ if ($canDisplay) {
 		}
 	}
 
+	$Pages = new Page();
+	$Pages->getPageTree(false, 0);
+	$PageList = $Pages->pageTree;
+	$_pagelist = array();
+	$PageDisabledValues = array();
+
+	if (count($PageList) > 0) {
+		foreach ($PageList as $page) {
+			$_pagelist[$page['ID']] = str_repeat('&nbsp;&nbsp;&nbsp;', $page['Depth']).$page['Title'];
+			if ((int) $page['Locked'] === 1) {
+				array_push($PageDisabledValues, $page['ID']);
+			}
+		}
+	}
+
 	$form = new Form ('form_project');
 	$form->method = 'POST';
 	$form->type = 'rows';
@@ -69,16 +95,18 @@ if ($canDisplay) {
 	if ($type === 'create') {
 		$form->addElement(new FormElement_Text("{_'forms_project_name'}", "name", array("required" => true)));
 		$form->addElement(new FormElement_Text("{_'forms_project_release_date'}", "release_date", array("required" => true)));
-		$form->addElement(new FormElement_Textarea("{_'forms_project_description'}", "description", array("required" => true)));	
+		$form->addElement(new FormElement_Textarea("{_'forms_project_description'}", "description", array("required" => true, "styleInput" => "height: 395px;")));	
 		$form->addElement(new FormElement_Select("{_'forms_project_owners'}", "owners[]", array("required" => true, "multiple" => true, "options" => $_userlist)));
 		$form->addElement(new FormElement_Select("{_'forms_project_editors'}", "editors[]", array("required" => false, "multiple" => true, "options" => $_userlist)));
+		$form->addElement(new FormElement_Select("{_'forms_project_pages'}", "pages[]", array("required" => false, "multiple" => true, "options" => $_pagelist, "disabled" => $PageDisabledValues)));			
 		$form->addElement(new FormElement_Submit(false, "submit_project", array("value" => "{_'forms_project_submit'}", "classInput" => "btn btn-block btn-primary")));
 	} else {
 		$form->addElement(new FormElement_Text("{_'forms_project_name'}", "name", array("required" => true, "value" => $Project['Name'])));
 		$form->addElement(new FormElement_Text("{_'forms_project_release_date'}", "release_date", array("required" => true, "value" => $Project['ReleaseDate'])));
-		$form->addElement(new FormElement_Textarea("{_'forms_project_description'}", "description", array("required" => true, "value" => $Project['Description'])));	
-		$form->addElement(new FormElement_Select("{_'forms_project_owners'}", "owners[]", array("required" => true, "multiple" => true, "options" => $_userlist, "selected" => $OwnerValues)));
+		$form->addElement(new FormElement_Textarea("{_'forms_project_description'}", "description", array("required" => true, "value" => $Project['Description'], "styleInput" => "height: 395px;")));	
+		$form->addElement(new FormElement_Select("{_'forms_project_owners'}", "owners[]", array("required" => true, "multiple" => true, "options" => $_userlist, "selected" => $OwnerValues)));		
 		$form->addElement(new FormElement_Select("{_'forms_project_editors'}", "editors[]", array("required" => false, "multiple" => true, "options" => $_userlist, "selected" => $EditorValues)));
+		$form->addElement(new FormElement_Select("{_'forms_project_pages'}", "pages[]", array("required" => false, "multiple" => true, "options" => $_pagelist, "selected" => $PageValues, "disabled" => $PageDisabledValues)));			
 		$form->addElement(new FormElement_Submit(false, "submit_project", array("value" => "{_'forms_project_submit_edit'}", "classInput" => "btn btn-block btn-primary")));			
 	}
 
@@ -100,13 +128,37 @@ if ($canDisplay) {
 
 		/* Project Owners */
 		$Project->owners = filter_input(INPUT_POST, "owners", FILTER_SANITIZE_STRING, FILTER_REQUIRE_ARRAY);
-		$Project->editors = filter_input(INPUT_POST, "editors", FILTER_SANITIZE_STRING, FILTER_REQUIRE_ARRAY);		
+		$Project->editors = filter_input(INPUT_POST, "editors", FILTER_SANITIZE_STRING, FILTER_REQUIRE_ARRAY);	
+
+		/* Pages */
+		$Pages = filter_input(INPUT_POST, "pages", FILTER_SANITIZE_STRING, FILTER_REQUIRE_ARRAY);				
+
+		$Page = new Page();
+		$Page->version = 0;
+		$PagesFailed = array();
 
 		switch ($type) {
 			case 'create':
 				if ($Project->createProject() && $Project->setProjectOwners()) {
+					if (!empty($Pages)) {
+						foreach ($Pages as $page) {
+							$page = (int) $page;
+							if (!$Page->isLocked($page)) {
+								$Page->lockPage($page);
+								$Page->clonePage($page, $Project->status);
+							} else {
+								array_push($PagesFailed, $page);
+							}
+						}
+					}
+
+					if (empty($PagesFailed)) {
+						$message = "{_'forms_project_form_created', sprintf([\"{$Project->name}\", \"/admin/projects/edit/{$Project->getCurrentProjectID()}\", \"#\"])}";
+					} else {
+						$message = "{_'forms_project_form_created_exception', sprintf([\"{$Project->name}\", \"/admin/projects/edit/{$Project->getCurrentProjectID()}\", \"#\", \"".implode(', ', $PagesFailed)."\"])}";
+					}
 					$Message = new Module();
-					$Message->addModule(new Message(), array("html" => "{_'forms_project_form_created', sprintf([\"{$Project->name}\", \"/admin/projects/edit/{$Project->getCurrentProjectID()}\", \"#\"])}", "class" => "alert-success", "OutputStyle" => $return["OutputStyle"], "OutputType" => $return["OutputType"], "Header" => 200));
+					$Message->addModule(new Message(), array("html" => $message, "class" => "alert-success", "OutputStyle" => $return["OutputStyle"], "OutputType" => $return["OutputType"], "Header" => 200));
 					$Message->output();	
 				} else {
 					$Message = new Module();
@@ -116,8 +168,26 @@ if ($canDisplay) {
 				break;
 			case 'edit':
 				if ($Project->updateProject() && $Project->setProjectOwners(true)) {
+					if (!empty($Pages)) {
+						foreach ($Pages as $page) {
+							$page = (int) $page;
+							if (!$Page->isLocked($page)) {
+								$Page->lockPage($page);
+								$Page->clonePage($page, $Project->id);
+							} else {
+								array_push($PagesFailed, $page);
+							}
+						}
+					}
+
+					if (empty($PagesFailed)) {
+						$message = "{_'forms_project_form_updated', sprintf([\"{$Project->name}\", \"#\"])}";
+					} else {
+						$message = "{_'forms_project_form_updated_exception', sprintf([\"{$Project->name}\", \"#\", \"".implode(', ', $PagesFailed)."\"])}";
+					}
+
 					$Message = new Module();
-					$Message->addModule(new Message(), array("html" => "{_'forms_project_form_updated', sprintf([\"{$Project->name}\", \"#\"])}", "class" => "alert-success", "OutputStyle" => $return["OutputStyle"], "OutputType" => $return["OutputType"], "Header" => 200));
+					$Message->addModule(new Message(), array("html" => $message, "class" => "alert-success", "OutputStyle" => $return["OutputStyle"], "OutputType" => $return["OutputType"], "Header" => 200));
 					$Message->output();
 				} else {
 					$Message = new Module();
