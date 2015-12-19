45237b6f7d32a5e24cd44b55b7a0d605ef796bc7
Vojtěch Václav Porteš <info@justart.org>
2015-12-19 02:24:33 +0100

    Update of page class
    Update of createPage, page attributes and attrs methods,
    createPageDetails, getPageById and more
    
    Addition of clonePage, isLocked, lockPage and unlockPage methods.

--- core/class/page.php
+++ core/class/page.php
@@ -4,7 +4,7 @@ Class Page Extends Minimal {
 
 	public $parent;
 	public $owner;
-	public $version;
+	public $version = 0;
 	public $locked;
 	public $visible;
 	public $weight;
@@ -14,6 +14,7 @@ Class Page Extends Minimal {
 	public $keywords;
 	public $template;
 	public $pageTree;
+	public $pageRefference = 0;
 	private $status;
 
 	private function getPageAttributes () {
@@ -27,6 +28,7 @@ Class Page Extends Minimal {
 			T_Pages.`Visible`,
 			T_Pages.`CreatedAt`,
 			T_Pages.`ModifiedAt`,
+			T_Pages.`PageRefference`,
 			T_PageDetails.`Title`";
 	}
 
@@ -41,6 +43,7 @@ Class Page Extends Minimal {
 			T_Pages.`Visible`,
 			T_Pages.`CreatedAt`,
 			T_Pages.`ModifiedAt`,
+			T_Pages.`PageRefference`,
 			T_PageDetails.`Name`,
 			T_PageDetails.`Title`,
 			T_PageDetails.`Description`,
@@ -50,14 +53,15 @@ Class Page Extends Minimal {
 
 	public function createPage () {
 		global $DB;
-		$Stm = $DB->prepare("INSERT INTO T_Pages (`Parent`, `Owner`, `Version`, `Locked`, `Visible`, `Weight`) VALUES (:Parent, :Owner, :Version, :Locked, :Visible, :Weight)");
+		$Stm = $DB->prepare("INSERT INTO T_Pages (`Parent`, `Owner`, `Version`, `Locked`, `Visible`, `Weight`, `PageRefference`) VALUES (:Parent, :Owner, :Version, :Locked, :Visible, :Weight, :PageRefference)");
 		$Stm->execute(array(
 			':Parent' => $this->parent,
 			':Owner' => $this->owner,
-			':Version' => 0,
+			':Version' => $this->version,
 			':Locked' => 0,
 			':Visible' => $this->visible,
-			':Weight' => $this->weight
+			':Weight' => $this->weight,
+			':PageRefference' => $this->pageRefference
 		));
 		$this->status = $DB->lastInsertId();
 		return $Stm->rowCount();
@@ -65,16 +69,101 @@ Class Page Extends Minimal {
 
 	public function createPageDetails () {
 		global $DB;
-		$Stm = $DB->prepare("INSERT INTO T_PageDetails (`Page`, `Name`, `Title`, `Description`, `Keywords`, `Template`) VALUES (:Page, :Name, :Title, :Description, :Keywords, :Template)");
+		$Stm = $DB->prepare("INSERT INTO T_PageDetails (`Page`, `Name`, `Title`, `Description`, `Keywords`, `Template`, `PageRefference`, `Version`) VALUES (:Page, :Name, :Title, :Description, :Keywords, :Template, :PageRefference, :Version)");
 		$Stm->execute(array(
 			':Page' => $this->status,
 			':Name' => $this->name,
 			':Title' => $this->title,
 			':Description' => $this->description,
 			':Keywords' => $this->keywords,
-			':Template' => $this->template
+			':Template' => $this->template,
+			':PageRefference' => $this->pageRefference,
+			':Version' => $this->version
 		));
 		return $Stm->rowCount();	
+	}
+
+
+	public function clonePage ($id = false, $newID = false) {
+		/* By setting newID to -1, initial backup will be created from 0 version of page,  */
+		if (isset($id)) {
+			if ($newID === -1) {
+				$id = $this->status;
+			}
+
+			$Page = $this->getPageByID($id);
+			$Clone = new Page();
+
+			/* Page and Page Details*/
+			$Clone->version = $newID;
+			$Clone->pageRefference = $id;
+
+			/* Page */
+			$Clone->parent = $Page['Parent'];
+			$Clone->owner = User::getUserID();
+			$Clone->visible = $Page['Visible'];
+			$Clone->weight = $Page['Weight'];
+
+			
+			/* Page Details */
+			$Clone->name = $Page['Name'];
+			$Clone->title = $Page['Title'];
+			$Clone->description = $Page['Description'];
+			$Clone->keywords = $Page['Keywords'];
+			$Clone->template = $Page['Template'];
+
+			$Clone->createPage();
+
+			/* Update status if neccessary */
+			if ($newID > 0) {	
+				$this->status = $id;
+			}
+
+			$Clone->createPageDetails();
+
+			return true;
+		}
+	}
+
+	public function isLocked($id) {
+		global $DB;
+		$Stm = $DB->prepare("SELECT T_Pages.`ID` FROM T_Pages WHERE T_Pages.`ID` = :ID AND T_Pages.`Version` = :Version AND T_Pages.`Locked` = :Locked LIMIT 1");
+		$Stm->execute(array(
+			':ID' => $id,
+			':Version' => $this->version,
+			':Locked' => 1
+		));
+		if ($Stm->rowCount() === 1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public function lockPage ($id) {
+		global $DB;
+		$Stm = $DB->prepare("UPDATE T_Pages SET
+			`Locked` = :Locked
+			WHERE T_Pages.`ID` = :ID AND T_Pages.`Version` = :Version");
+		$Stm->execute(array(
+			':Locked' => 1,
+			':ID' => $id,
+			':Version' => $this->version
+		));
+		return $Stm->rowCount();
+	}
+
+	public function unlockPage ($id) {
+		global $DB;
+		$Stm = $DB->prepare("UPDATE T_Pages SET
+			`Locked` = :Locked
+			WHERE T_Pages.`ID` = :ID AND T_Pages.`Version` = :Version");
+		$Stm->execute(array(
+			':Locked' => 0,
+			':ID' => $id,
+			':Version' => $this->version
+		));
+		return $Stm->rowCount();
 	}
 
 	public function getPageByID ($id) {
@@ -83,9 +172,10 @@ Class Page Extends Minimal {
 		{$this->getPageAttributesDetailed()}
 		FROM T_Pages
 		LEFT JOIN T_PageDetails
-		ON T_Pages.`ID` = T_PageDetails.`Page` WHERE T_Pages.`ID` = :ID LIMIT 1");			
+		ON T_Pages.`ID` = T_PageDetails.`Page` WHERE T_Pages.`ID` = :ID AND T_Pages.`Version` = :Version LIMIT 1");			
 		$Stm->execute(array(
-			':ID' => $id
+			':ID' => $id,
+			':Version' => $this->version
 		));
 		return $Stm->fetch(PDO::FETCH_ASSOC);			
 	}
@@ -96,9 +186,10 @@ Class Page Extends Minimal {
 		{$this->getPageAttributesDetailed()}
 		FROM T_Pages
 		LEFT JOIN T_PageDetails
-		ON T_Pages.`ID` = T_PageDetails.`Page` WHERE T_Pages.`Parent` = :Parent LIMIT 1");			
+		ON T_Pages.`ID` = T_PageDetails.`Page` AND T_Pages.`Version` = T_PagesDetails.`Version` WHERE T_Pages.`Parent` = :Parent AND T_Pages.`Version` = :Version LIMIT 1");			
 		$Stm->execute(array(
-			':Parent' => $id
+			':Parent' => $id,
+			':Version' => $this->version
 		));
 		return $Stm->fetch(PDO::FETCH_ASSOC);			
 	}
@@ -108,12 +199,13 @@ Class Page Extends Minimal {
 		global $DB, $M;
 		$Stm = $DB->prepare("SELECT
 		{$this->getPageAttributes()},
-		(SELECT COUNT(T_PagesB.`ID`) FROM T_Pages T_PagesB WHERE T_Pages.`ID` = T_PagesB.`Parent`) AS numChildPages		
+		(SELECT COUNT(T_PagesB.`ID`) FROM T_Pages T_PagesB WHERE T_Pages.`ID` = T_PagesB.`Parent` AND T_Pages.`Version` = T_PagesB.`Version`) AS numChildPages		
 		FROM T_Pages
 		LEFT JOIN T_PageDetails
-		ON (T_Pages.`ID` = T_PageDetails.`Page`) WHERE T_Pages.`Parent` = :Parent");
+		ON (T_Pages.`ID` = T_PageDetails.`Page` AND T_Pages.`Version` = T_PageDetails.`Version`) WHERE T_Pages.`Parent` = :Parent AND T_Pages.`Version` = :Version");
 		$Stm->execute(array(
-			':Parent' => $parent
+			':Parent' => $parent,
+			':Version' => $this->version
 		));
 		return $Stm->fetchAll(PDO::FETCH_ASSOC);
 	}
@@ -122,12 +214,13 @@ Class Page Extends Minimal {
 		global $DB;
 		$Stm = $DB->prepare("SELECT
 		{$this->getPageAttributesDetailed()},
-		(SELECT COUNT(T_PagesB.`ID`) FROM T_Pages T_PagesB WHERE T_Pages.`ID` = T_PagesB.`Parent`) AS numChildPages
+		(SELECT COUNT(T_PagesB.`ID`) FROM T_Pages T_PagesB WHERE T_Pages.`ID` = T_PagesB.`Parent` AND T_Pages.`Version` = T_PagesB.`Version`) AS numChildPages
 		FROM T_Pages
 		LEFT JOIN T_PageDetails
-		ON T_Pages.`ID` = T_PageDetails.`Page` WHERE T_Pages.`Parent` = :Parent");
+		ON T_Pages.`ID` = T_PageDetails.`Page` WHERE T_Pages.`Parent` = :Parent AND T_Pages.`Version` = :Version");
 		$Stm->execute(array(
-			':Parent' => $parent
+			':Parent' => $parent,
+			':Version' => $this->version
 		));
 		return $Stm->fetchAll(PDO::FETCH_ASSOC);		
 	}
@@ -149,21 +242,6 @@ Class Page Extends Minimal {
 			}
 		}
 		$depth = $depth -1;
-	}
-
-	public function clonePage ($id, $newID = false) {
-		/* By setting newID to -1, initial backup will be created from 0 version of page,  */
-		if (isset($id)) {
-			$Page = $this->getPageByID($id);
-			$Clone = new Page();
-			$Clone->parent = $Page['Parent'];
-			$Clone->owner = User::getUserID();
-			$Clone->version = $newID;
-			$Clone->locked = 1;
-			$Clone->visible = $Page['Visible'];
-			$Clone->weight = $Page['Weight'];
-			
-		}
 	}
 } 
 
