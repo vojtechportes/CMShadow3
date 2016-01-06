4ea5dcc1fb72bfff04e57a4ca82196a0c34bf237
Vojtěch Václav Porteš <info@justart.org>
2016-01-06 20:19:06 +0100

    Addition of layouts stringtable

---
+++ core/stringtable/stringtable_layouts.php
@@ -0,0 +1,13 @@
+<?php
+
+$Stringtable = array(
+	/* Layout List */
+    'layouts_layout_list_th_id' => 'ID',     
+    'layouts_layout_list_th_name' => 'Name',
+    'layouts_layout_list_th_createdAt' => 'Created At',
+    'layouts_layout_list_th_modifiedAt' => 'Modified At',
+    'layouts_layout_list_th_actions' => 'Actions',
+    'layouts_controls_create_layout' => 'Create Layout'
+);
+
+?>
