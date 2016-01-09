f005bb957ff884cb9a89f4f3c287dda6e6a39e9b
Vojtěch Václav Porteš <info@justart.org>
2016-01-09 19:18:06 +0100

    Addition of layoutform.create js module

---
+++ style/script/application-modules/layoutform.create.js
@@ -0,0 +1,13 @@
+(function(){
+	var reload = function () {
+		var parent = '.layoutForm[data-api-load]';
+		$.each($(parent), function(k, el){
+			var parentData = $('.layoutForm' + parent).data('api-load');
+			APICommands.call($(el), parentData, true);
+		});		
+	}
+
+	$(window).on('apiReloadForce.admin/layout/api/form', function(){
+		reload();
+	});	
+})();
