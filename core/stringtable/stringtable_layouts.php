ed4864c3b5abadef7458d25294907d1ce64072ea
Vojtěch Václav Porteš <info@justart.org>
2016-01-09 18:47:30 +0100

    stringtable layouts update

--- core/stringtable/stringtable_layouts.php
+++ core/stringtable/stringtable_layouts.php
@@ -7,7 +7,14 @@ $Stringtable = array(
     'layouts_layout_list_th_createdAt' => 'Created At',
     'layouts_layout_list_th_modifiedAt' => 'Modified At',
     'layouts_layout_list_th_actions' => 'Actions',
-    'layouts_controls_create_layout' => 'Create Layout'
+    'layouts_controls_create_layout' => 'Create Layout',
+
+    /**/
+
+    'layouts_layout_delete' => 'Delete layout',
+    'layouts_layout_delete_message_question' => '<p>Are you sure you want to delete layout "%s"? If layout is used in template, it will be removed also from tempalte and you will need to assign new one.</p>',
+    'layouts_layout_delete_message_success' => '<p>Layout "%s" was successfully deleted.</p>',
+    'layouts_layout_delete_message_error' => '<p>Unexpected error occured. Layout "%s" was not deleted. Please <a data-api-reload href="%s">try it again later</a></p>'
 );
 
 ?>
