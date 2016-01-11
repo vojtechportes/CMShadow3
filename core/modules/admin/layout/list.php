fe7445cbdeb532e230d8c5a5173fb8233c867e2d
Vojtěch Václav Porteš <info@justart.org>
2016-01-11 19:49:05 +0100

    Removal of err reporting

--- core/modules/admin/layout/list.php
+++ core/modules/admin/layout/list.php
@@ -1,9 +1,5 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 $Module = new Module();
 $Module->addModule(new LayoutList('getLayoutList'), $return);
 $Module->output();
