453dab9603a922b9b339e45cfd5e9f686cbe7317
Vojtěch Václav Porteš <info@justart.org>
2016-01-13 19:52:49 +0100

    Update of form.slot.php api module

--- core/modules/admin/layout/api/form.slot.php
+++ core/modules/admin/layout/api/form.slot.php
@@ -16,15 +16,22 @@ $canDisplay = true;
 $projectExist = true;
 $type = 'create';
 
+if (!array_key_exists('Path', $return))
+	$return['Path'] = $Path;
+
 if (array_key_exists('type', $return)) {
 	if ($return['type'] === 'edit') {
 		$type = 'edit';
 		$return = concat_property('class', ' in', $return);
 		$id = $M->extractID($return['Path']);
-		if ($id) {
-			/*load slot info*/
-		}
+
+		$Slot = new Layout();
+		$Slot = $Slot->getLayoutSlotByID((int) $return['slotId']);
+	} else if ($return['type'] === 'create') {
+		$id = $M->extractID($return['Path']);	
 	}
+} else { 
+	$id = $M->extractID($return['Path']);	
 }
 
 if ($canDisplay) {
@@ -39,7 +46,14 @@ if ($canDisplay) {
 		$Message->output();
 	}
 
+	$Layout = new Layout();
+	$Layout->getLayoutSlotsTree(0, $id);
+	$Tree = $Layout->slotsTree;
 	$slots = array("0" => "{_'forms_layout_slot_parent_root'}");
+
+	foreach ($Tree as $slot) {
+		$slots[$slot['ID']] = str_repeat('. ', $slot['Depth']).$slot['Name']; 
+	}
 
 	$form = new Form ('form_layout');
 	$form->method = 'POST';
@@ -49,85 +63,92 @@ if ($canDisplay) {
 
 	if ($type === 'create') {
 		$form->addElement(new FormElement_Text("{_'forms_layout_slot_name'}", "name", array("required" => true)));
-		$form->addElement(new FormElement_Select("{_'forms_layout_slot_parent'}", "parent[]", array("required" => true, "options" => $slots)));
+		$form->addElement(new FormElement_Select("{_'forms_layout_slot_parent'}", "parent", array("required" => true, "options" => $slots)));
 		$form->addElement(new FormElement_Text("{_'forms_layout_slot_weight'}", "weight", array("required" => false)));
 		$form->addElement(new FormElement_Text("{_'forms_layout_slot_class'}", "class", array("required" => false)));
 		$form->addElement(new FormElement_Text("{_'forms_layout_slot_identifier'}", "identifier", array("required" => false)));
 		$form->addElement(new FormElement_Text("{_'forms_layout_slot_data'}", "data", array("required" => false)));
 
-		$form->addElement(new FormElement_Submit(false, "submit_project", array("value" => "{_'forms_project_submit'}", "classInput" => "btn btn-block btn-primary")));
+		$form->addElement(new FormElement_Submit(false, "submit_project", array("value" => "{_'forms_layout_slot_submit'}", "classInput" => "btn btn-block btn-primary")));
 	} else {
-		/* Edit fields*/
+		$form->addElement(new FormElement_Text("{_'forms_layout_slot_name'}", "name", array("required" => true, "value" => $Slot['Name'])));
+		$form->addElement(new FormElement_Select("{_'forms_layout_slot_parent'}", "parent", array("required" => true, "options" => $slots, "selected" => array($Slot['Parent']), "disabled" => array($Slot['ID']))));
+		$form->addElement(new FormElement_Text("{_'forms_layout_slot_weight'}", "weight", array("required" => false, "value" => $Slot['Weight'])));
+		$form->addElement(new FormElement_Text("{_'forms_layout_slot_class'}", "class", array("required" => false, "value" => $Slot['Class'])));
+		$form->addElement(new FormElement_Text("{_'forms_layout_slot_identifier'}", "identifier", array("required" => false, "value" => $Slot['Identifier'])));
+		$form->addElement(new FormElement_Text("{_'forms_layout_slot_data'}", "data", array("required" => false, "value" => $Slot['Data'])));
+
+		$form->addElement(new FormElement_Submit(false, "submit_project", array("value" => "{_'forms_layout_slot_submit_edit'}", "classInput" => "btn btn-block btn-primary")));
 	}
-
-
 
 	$Output = $form->output($return);
 
 	if ($Output) {
-		$Project = new Project();
+		$Layout = new Layout();
 
-		/* Project */
+		/* Layout Slot */
 
-		if ($type === 'edit')
-			$Project->id = $id;
+		if ($type === 'edit') {
+			$Layout->id = $Slot['ID'];
+		}
+			
+		$Layout->layout = $id;
+		$Layout->parent = filter_input(INPUT_POST, "parent", FILTER_SANITIZE_NUMBER_INT);
+		$Layout->weight = filter_input(INPUT_POST, "weight", FILTER_SANITIZE_NUMBER_INT);
+		$Layout->slotName = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
+		$Layout->class = filter_input(INPUT_POST, "class", FILTER_SANITIZE_STRING);
+		$Layout->identifier = filter_input(INPUT_POST, "identifier", FILTER_SANITIZE_STRING);
+		$Layout->data = filter_input(INPUT_POST, "data", FILTER_SANITIZE_STRING);
 
-		$Project->name = filter_input(INPUT_POST, "name");
-		$Project->description = filter_input(INPUT_POST, "description");
-		$Project->relese_date = filter_input(INPUT_POST, "release_date");
+		$Parent = '';
+		if ($Layout->parent !== 0) {
+			$Parent = $Layout->getLayoutSlotByID($Layout->parent);
+			$ParentPath = $Parent['Path'];
+		}
 
-		/* Project Owners */
-		$Project->owners = filter_input(INPUT_POST, "owners", FILTER_SANITIZE_STRING, FILTER_REQUIRE_ARRAY);
-		$Project->editors = filter_input(INPUT_POST, "editors", FILTER_SANITIZE_STRING, FILTER_REQUIRE_ARRAY);	
+		$Layout->path = $ParentPath.get_seo_name($Layout->slotName).'--';
 
-		/* Pages */
-		$Pages = filter_input(INPUT_POST, "pages", FILTER_SANITIZE_STRING, FILTER_REQUIRE_ARRAY);				
-
-		$Page = new Page();
-		$Page->version = 0;
-		$PagesFailed = array();
+		if ($type === 'edit') {
+			$changeChildPath = false;
+			if ($Layout->path === $Slot['Path']) {
+				$Layout->path = $Slot['Path'];
+			} else {
+				if ((int) $Slot['numChildSlots'] > 0) {
+					$changeChildPath = true;
+				}
+			}
+		}
 
 		switch ($type) {
 			case 'create':
-				if (/*$Project->createProject() && $Project->setProjectOwners()*/ false) {
-
-					if (empty($PagesFailed)) {
-						$message = "{_'forms_project_form_created', sprintf([\"{$Project->name}\", \"/admin/projects/edit/{$Project->getCurrentProjectID()}\", \"#\"])}";
-					} else {
-						$message = "{_'forms_project_form_created_exception', sprintf([\"{$Project->name}\", \"/admin/projects/edit/{$Project->getCurrentProjectID()}\", \"#\", \"".implode(', ', $PagesFailed)."\"])}";
-					}
+				if ($Layout->createLayoutSlot()) {
 					$Message = new Module();
-					$Message->addModule(new Message(), array("html" => $message, "class" => "alert-success", "OutputStyle" => $return["OutputStyle"], "OutputType" => $return["OutputType"], "Header" => 200));
+					$Message->addModule(new Message(), array("html" => "{_'forms_layout_slot_form_success', sprintf([\"{$Layout->slotName}\", \"#\", \"#\"])}", "class" => "alert-success", "OutputStyle" => $return["OutputStyle"], "OutputType" => $return["OutputType"], "Header" => 200));
 					$Message->output();	
 				} else {
 					$Message = new Module();
-					$Message->addModule(new Message(), array("html" => "{_'forms_project_form_error', sprintf([\"{$Project->name}\", \"#\"])}", "class" => "alert-danger", "OutputStyle" => $return["OutputStyle"], "OutputType" => $return["OutputType"], "Header" => 200));
+					$Message->addModule(new Message(), array("html" => "{_'forms_layout_slot_form_error', sprintf([\"{$Layout->slotName}\", \"#\"])}", "class" => "alert-danger", "OutputStyle" => $return["OutputStyle"], "OutputType" => $return["OutputType"], "Header" => 200));
 					$Message->output();	
 				}
 				break;
 			case 'edit':
-				if ($Project->updateProject() && $Project->setProjectOwners(true)) {
-					if (!empty($Pages)) {
-						foreach ($Pages as $page) {
-							$page = (int) $page;
-							if (!$Page->isLocked($page)) {
-								$Page->lockPage($page);
-								$Page->clonePage($page, $Project->id);
-							} else {
-								array_push($PagesFailed, $page);
-							}
+				
+				if ($Layout->updateLayoutSlot()) {
+					if ($changeChildPath) {
+						if($Layout->updateLayoutSlotChildPaths()) {
+							$Message = new Module();
+							$Message->addModule(new Message(), array("html" => "{_'forms_layout_slot_form_update_success', sprintf([\"{$Layout->slotName}\", \"#\", \"#\"])}", "class" => "alert-success", "OutputStyle" => $return["OutputStyle"], "OutputType" => $return["OutputType"], "Header" => 200));
+							$Message->output();				
+						} else {
+							$Message = new Module();
+							$Message->addModule(new Message(), array("html" => "{_'forms_layout_slot_form_update_exception', sprintf([\"{$Project->name}\", \"#\"])}", "class" => "alert-danger", "OutputStyle" => $return["OutputStyle"], "OutputType" => $return["OutputType"], "Header" => 200));
+							$Message->output();		
 						}
-					}
-
-					if (empty($PagesFailed)) {
-						$message = "{_'forms_project_form_updated', sprintf([\"{$Project->name}\", \"#\"])}";
 					} else {
-						$message = "{_'forms_project_form_updated_exception', sprintf([\"{$Project->name}\", \"#\", \"".implode(', ', $PagesFailed)."\"])}";
+						$Message = new Module();
+						$Message->addModule(new Message(), array("html" => "{_'forms_layout_slot_form_update_success', sprintf([\"{$Layout->slotName}\", \"#\", \"#\"])}", "class" => "alert-success", "OutputStyle" => $return["OutputStyle"], "OutputType" => $return["OutputType"], "Header" => 200));
+						$Message->output();	
 					}
-
-					$Message = new Module();
-					$Message->addModule(new Message(), array("html" => $message, "class" => "alert-success", "OutputStyle" => $return["OutputStyle"], "OutputType" => $return["OutputType"], "Header" => 200));
-					$Message->output();
 				} else {
 					$Message = new Module();
 					$Message->addModule(new Message(), array("html" => "{_'forms_project_form_update_error', sprintf([\"{$Project->name}\", \"#\"])}", "class" => "alert-danger", "OutputStyle" => $return["OutputStyle"], "OutputType" => $return["OutputType"], "Header" => 200));
