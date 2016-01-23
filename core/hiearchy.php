b10fb2a1a272c58d23475c4922a1d16a617c7696
Vojtěch Václav Porteš <info@justart.org>
2016-01-23 20:09:30 +0100

    Hiearchy update by template list, controls module loaders

--- core/hiearchy.php
+++ core/hiearchy.php
@@ -130,7 +130,8 @@ $Hiearchy = array(
 			),
 			"Content" => array(
 				"main" => array(
-
+					array("module" => "admin/template/api/controls.loader", "dependencies" => array()),
+					array("module" => "admin/template/api/list.loader")
 				)
 			)
 		),
