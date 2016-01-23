2826eb9208a9546c4c79503f004f4b93136aa64d
Vojtěch Václav Porteš <info@justart.org>
2016-01-23 20:07:51 +0100

    Addition of stringtable_templates

---
+++ core/stringtable/stringtable_templates.php
@@ -0,0 +1,15 @@
+<?php
+
+$Stringtable = array(
+	/* Layout List */
+    'templates_template_list_th_id' => 'ID',     
+    'templates_template_list_th_name' => 'Name',
+    'templates_template_list_th_createdAt' => 'Created At',
+    'templates_template_list_th_modifiedAt' => 'Modified At',
+    'templates_template_list_th_actions' => 'Actions',
+
+    /* Controls */
+    'templates_controls_create_template' => 'Create Template'
+);
+
+?>
