d7a593d600c9d1288e6cb6c5eb7056a101a2dcd8
Vojtěch Václav Porteš <info@justart.org>
2016-01-23 15:09:43 +0100

    Addition of IDSRegister

---
+++ core/phpids/Register.php
@@ -0,0 +1,20 @@
+<?php
+
+$IDSRegister = array(
+    'IDS\Converter' => '/core/phpids/Converter.php',
+    'IDS\Event' => '/core/phpids/Event.php',
+    'IDS\Filter' => '/core/phpids/Filter.php',
+    'IDS\Init' => '/core/phpids/Init.php',
+    'IDS\Monitor' => '/core/phpids/Monitor.php',
+    'IDS\Report' => '/core/phpids/Report.php',
+    'IDS\Version' => '/core/phpids/Version.php',
+    'IDS\Caching\AppCache' => '/core/phpids/Caching/AppCache.php',
+    'IDS\Caching\CacheFactory' => '/core/phpids/Caching/CacheFactory.php',
+    'IDS\Caching\CacheInterface' => '/core/phpids/Caching/CacheInterface.php',
+    'IDS\Caching\DatabaseCache' => '/core/phpids/Caching/DatabaseCache.php',
+    'IDS\Caching\FileCache' => '/core/phpids/Caching/FileCache.php',
+    'IDS\Caching\SessionCache' => '/core/phpids/Caching/SessionCache.php',
+    'IDS\Filter\Storage' => '/core/phpids/Filter/Storage.php'
+);
+
+?>
