c9861d8238f8c0be472691057424d60409cd3869
Vojtěch Václav Porteš <info@justart.org>
2016-01-24 23:33:11 +0100

    FileSystem class update

--- core/class/filesystem.php
+++ core/class/filesystem.php
@@ -13,6 +13,8 @@ Class FileSystem Extends Minimal {
 	public $separator = '/';
 	public $filterFiles;
 	public $filterFolders;
+	public $skipRoot = false;
+	public $maxDepth = 0; /* 0 = unlimited */
 
 	public function __construct ($filterFolders, $filterFiles) {
 		$this->filterFolders = $filterFolders;
@@ -41,22 +43,28 @@ Class FileSystem Extends Minimal {
 	   return $result; 
 	}
 
-	private function filterFolders ($array) {
+	private function filterFolders ($array, $depth) {
+		$depth++;
+
 		foreach ($array as $key => $value) {
 			if (is_array($array[$key]))	{
 				if ($this->pathStrip)
-					$key = str_replace($this->path.'/', '', $key);				
-				$this->folders[] = $key;
-				self::filterFolders($array[$key]);
+					$_key = str_replace($this->path.'/', '', $key);				
+				$this->folders[] = $_key;
+
+				if ($this->maxDepth === 0 || $depth <= $this->maxDepth) {
+					self::filterFolders($array[$key], $depth);
+				}
 			}						
 		}
-
 	}
 
-	private function filterFiles ($array, $file) {
+	private function filterFiles ($array, $file, $depth) {
 		foreach ($array as $key => $value) {
 			if (is_array($array[$key]))	{
-				self::filterFiles($array[$key], $file);
+				if ($this->maxDepth === 0 || $depth <= $this->maxDepth) {
+					self::filterFiles($array[$key], $file);
+				}
 			} else {
 				if (@preg_match("/".$file."/i", $value)) {
 					if ($this->pathStrip)
@@ -79,17 +87,17 @@ Class FileSystem Extends Minimal {
 
 		$this->content = self::getContent($this->path);
 		if ($this->filterFolders) {
-			self::filterFolders($this->content);
-			array_unshift($this->folders, $this->path);
-			$this->content = $this->folders;
+			self::filterFolders($this->content, 1);
+			if (!$this->skipRoot) {
+				array_unshift($this->folders, $this->path);
+			}
 		}
 
 		if ($this->filterFiles) {
-			self::filterFiles($this->content, $this->fileMask);
-			$this->content = $this->files;
+			self::filterFiles($this->content, $this->fileMask, 1);
 		}
 
-		return $this->content;
+		return array_merge($this->files, $this->folders);
 	}
 
 }
