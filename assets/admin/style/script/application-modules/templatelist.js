f5feb813e9bf478a7e253501b81b815b3ec037ce
Vojtěch Václav Porteš <info@justart.org>
2016-01-25 17:37:49 +0100

    Addition of templatelist.js

---
+++ assets/admin/style/script/application-modules/templatelist.js
@@ -0,0 +1,80 @@
+(function(){
+	var remove = function () {
+		var $elements = $('.templateDelete[data-api-load] [data-delete-confirm]');
+
+		if ($elements.length > 0) {
+			$elements.off('click.delete.confirm').on('click.delete.confirm', function(e){
+				e.preventDefault();
+				e.stopPropagation();
+
+				var $this = $(this),
+					$parent = $this.parents('[data-api-load]'),
+					parentData = $.extend({}, $parent.data('api-load'));
+					parentData['arguments'] = $.extend({}, parentData['arguments'], {'deleteConfirm': 1});
+
+				APICommands.call($parent, parentData, true, false, function(){
+					if (typeof parentData['arguments']['dependencies'] !== 'undefined') {
+						$.each(parentData['arguments']['dependencies'], function(k, module){
+							console.log(module);
+							if (module === 'admin/form') {
+								$(window).trigger('apiReload.' + module);
+							} else {
+								$(window).trigger('apiReloadForce.' + module);
+							}
+						});
+					}				
+				});
+			});
+		}
+	}
+
+	var update = function () {
+		var parent = '.templateList[data-api-load]',
+			actions = {'edit': '[data-edit]', 'delete': '[data-delete]'};
+
+		if ($(parent).find(actions.delete).length > 0) {
+			var $action = $(parent).find(actions.delete);
+			
+			$action.off('click.delete').on('click.delete', function(e){
+				e.preventDefault();
+
+				var $this = $(this),
+					arguments = $this.data('arguments');
+					$parent = $($this.data('target')).find('[data-api-load]');
+				
+				if ($parent.length > 0) {
+					var parentData = $.extend({}, $parent.data('api-load'));
+						parentData['arguments'] = $.extend({}, parentData['arguments'], arguments);
+
+					$parent.data('api-load', parentData);
+
+					APICommands.call($parent, parentData, true);
+				}
+			});
+		}
+	}
+
+	var reload = function () {
+		var parent = '.templateList[data-api-load]';
+		$.each($(parent), function(k, el){
+			var parentData = $('.templateList' + parent).data('api-load');
+			APICommands.call($(el), parentData, true);
+		});		
+	}
+
+	$(document).ready(function(){
+		update();
+	});
+
+	$(window).on('apiReload.admin/template/list', function(){
+		update();
+	});	
+
+	$(window).on('apiReloadForce.admin/template/list', function(){
+		reload();
+	});
+
+	$(window).on('apiReload.admin/template/delete', function(){
+		remove();
+	});
+})();
